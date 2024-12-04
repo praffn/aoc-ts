a46911c41e475019f9224052ca1285a54a08ab00
Phillip <phillip@praffn.dk>
2024-12-04 15:22:06 +0100

    feat: solve 2015 day 14

---
+++ src/solvers/2015/14.test.ts
@@ -0,0 +1,14 @@
+import test from "node:test";
+import { createLineReaderFromString } from "../../line-reader";
+import solver from "./14";
+
+test("2015.13", async (t) => {
+  const input = `Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.`;
+
+  const lineReader = createLineReaderFromString(input);
+  const result = await solver(lineReader);
+
+  t.assert.equal(result.first, 2660);
+  t.assert.equal(result.second, 1564);
+});
---
+++ src/solvers/2015/14.ts
@@ -0,0 +1,65 @@
+import { createSolver } from "../../solution";
+
+interface Reindeer {
+  name: string;
+  speed: number;
+  flyTime: number;
+  restTime: number;
+}
+
+function parseReindeer(input: string): Reindeer {
+  const parts = input.split(" ");
+  const name = parts[0];
+  const speed = Number.parseInt(parts[3], 10);
+  const flyTime = Number.parseInt(parts[6], 10);
+  const restTime = Number.parseInt(parts[13], 10);
+
+  return {
+    name,
+    speed,
+    flyTime,
+    restTime,
+  };
+}
+
+function computeDistanceAtSecond(reindeer: Reindeer, second: number): number {
+  const cycleTime = reindeer.flyTime + reindeer.restTime;
+  const cycles = Math.floor(second / cycleTime);
+  const remaining = second % cycleTime;
+
+  const flyTime = Math.min(reindeer.flyTime, remaining);
+  return cycles * reindeer.speed * reindeer.flyTime + flyTime * reindeer.speed;
+}
+
+export default createSolver(async (input) => {
+  const reindeers: Array<Reindeer> = [];
+
+  for await (const line of input) {
+    reindeers.push(parseReindeer(line));
+  }
+
+  const distance = Array(reindeers.length).fill(0);
+  const scoring = Array(reindeers.length).fill(0);
+
+  for (let second = 0; second < 2503; second++) {
+    let maxDistance = -Infinity;
+
+    for (let i = 0; i < reindeers.length; i++) {
+      distance[i] = computeDistanceAtSecond(reindeers[i], second + 1);
+      maxDistance = Math.max(maxDistance, distance[i]);
+    }
+
+    for (let i = 0; i < reindeers.length; i++) {
+      if (distance[i] === maxDistance) {
+        scoring[i]++;
+      }
+    }
+  }
+
+  console.log(scoring);
+
+  return {
+    first: Math.max(...distance),
+    second: Math.max(...scoring),
+  };
+});
