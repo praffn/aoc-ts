377dc2858c768c53ca22c3c981ef1bc623b19146
Phillip <phillip@praffn.dk>
2024-12-13 12:59:46 +0100

    feat: solve 2024 day 13

---
+++ src/solvers/2024/13.test.ts
@@ -0,0 +1,27 @@
+import test from "node:test";
+import { createLineReaderFromString } from "../../line-reader";
+import solver from "./13";
+
+test("2024.13", async (t) => {
+  const input = `Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279`;
+
+  const lineReader = createLineReaderFromString(input);
+  const result = await solver(lineReader);
+
+  t.assert.equal(result.first, 480);
+  t.assert.equal(result.second, 875318608908);
+});
---
+++ src/solvers/2024/13.ts
@@ -0,0 +1,61 @@
+import { add, makeVec2, type Vec2 } from "../../lib/linalg/vec2";
+import { createSolverWithLineArray } from "../../solution";
+
+function solveDiophantineEquation(
+  [ax, ay]: Vec2,
+  [bx, by]: Vec2,
+  [cx, cy]: Vec2
+): [number, number] | undefined {
+  const det = ax * by - ay * bx;
+  if (det === 0) {
+    return;
+  }
+
+  const detM = cx * by - cy * bx;
+  const detN = ax * cy - ay * cx;
+
+  const m = detM / det;
+  const n = detN / det;
+
+  if (Number.isInteger(m) && Number.isInteger(n)) {
+    return [m, n];
+  }
+}
+
+const re = /([+-]?\d+)/g;
+
+export default createSolverWithLineArray(async (input) => {
+  const machines: Array<[Vec2, Vec2, Vec2]> = [];
+
+  for (let i = 0; i < input.length; i += 4) {
+    const [ax, ay] = input[i + 0].matchAll(re);
+    const [bx, by] = input[i + 1].matchAll(re);
+    const [cx, cy] = input[i + 2].matchAll(re);
+
+    machines.push([
+      makeVec2(Number.parseInt(ax[0]), Number.parseInt(ay[0])),
+      makeVec2(Number.parseInt(bx[0]), Number.parseInt(by[0])),
+      makeVec2(Number.parseInt(cx[0]), Number.parseInt(cy[0])),
+    ]);
+  }
+
+  const addVec = makeVec2(10_000_000_000_000);
+
+  let minTokensFirst = 0;
+  let minTokensSecond = 0;
+  for (const [a, b, c] of machines) {
+    const solution1 = solveDiophantineEquation(a, b, c);
+    if (solution1) {
+      minTokensFirst += solution1[0] * 3 + solution1[1];
+    }
+    const solution2 = solveDiophantineEquation(a, b, add(c, addVec));
+    if (solution2) {
+      minTokensSecond += solution2[0] * 3 + solution2[1];
+    }
+  }
+
+  return {
+    first: minTokensFirst,
+    second: minTokensSecond,
+  };
+});
