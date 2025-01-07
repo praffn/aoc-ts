d6f9e9a5f832413d176777d45c9689d33ba0d461
Phillip <phillip@praffn.dk>
2025-01-07 15:12:30 +0100

    feat: solve 2018 day 10

---
+++ src/solvers/2018/10.test.ts
@@ -0,0 +1,5 @@
+import test from "node:test";
+
+test("2018.10", async (t) => {
+  t.skip("Skipped because it is hard to test");
+});
---
+++ src/solvers/2018/10.ts
@@ -0,0 +1,74 @@
+import { Grid2D } from "../../lib/grid/grid2d";
+import { add, makeVec2, scale, type Vec2 } from "../../lib/linalg/vec2";
+import { createSolverWithLineArray } from "../../solution";
+
+type Point = {
+  position: Vec2;
+  velocity: Vec2;
+};
+
+const n = (s: string) => Number.parseInt(s);
+const re = /position=<\s*(-?\d+),\s*(-?\d+)> velocity=<\s*(-?\d+),\s*(-?\d+)>/;
+function parsePoint(line: string): Point {
+  const [, px, py, vx, vy] = line.match(re)!;
+  return {
+    position: makeVec2(n(px), n(py)),
+    velocity: makeVec2(n(vx), n(vy)),
+  };
+}
+
+function print(
+  points: Array<Point>,
+  t: number,
+  dimensions: { width: number; height: number }
+) {
+  const positions = points.map(({ position, velocity }) =>
+    add(position, scale(velocity, t))
+  );
+  const grid = new Grid2D(dimensions.width, dimensions.height, " ");
+  const minX = Math.min(...positions.map((p) => p.x));
+  const minY = Math.min(...positions.map((p) => p.y));
+
+  for (const pos of positions) {
+    grid.set(pos.x - minX, pos.y - minY, "#");
+  }
+
+  console.log(grid.toString());
+}
+
+export default createSolverWithLineArray(async (input) => {
+  const points = input.map(parsePoint);
+
+  let smallestArea = Infinity;
+  let smallestDimensions = { width: 0, height: 0 };
+  let smallestAreaTime = 0;
+
+  for (let i = 10_000; i < 10_0000; i++) {
+    const positions = points.map(({ position, velocity }) =>
+      add(position, scale(velocity, i))
+    );
+
+    const minX = Math.min(...positions.map((p) => p.x));
+    const maxX = Math.max(...positions.map((p) => p.x));
+    const minY = Math.min(...positions.map((p) => p.y));
+    const maxY = Math.max(...positions.map((p) => p.y));
+
+    const width = maxX - minX + 1;
+    const height = maxY - minY + 1;
+
+    const area = width * height;
+    if (area < smallestArea) {
+      smallestArea = area;
+      smallestDimensions = { width, height };
+      smallestAreaTime = i;
+    }
+  }
+
+  // Uncomment line below to print message for part 1
+  // print(points, smallestAreaTime, smallestDimensions);
+
+  return {
+    first: "Uncomment code to see solution",
+    second: smallestAreaTime,
+  };
+});
