1e2c57645f2e59557cb521ec604dabca7f547e4d
Phillip <phillip@praffn.dk>
2025-01-17 16:07:40 +0100

    add relative mode to intcode cpu

--- src/solvers/2019/intcode.ts
+++ src/solvers/2019/intcode.ts
@@ -1,8 +1,9 @@
 export type Memory = Array<number>;
 
-type Mode = 0 | 1;
+type Mode = 0 | 1 | 2;
 const MODE_POSITION: Mode = 0;
 const MODE_IMMEDIATE: Mode = 1;
+const MODE_RELATIVE: Mode = 2;
 
 export type HaltType = "terminated" | "waiting" | "error";
 export const HALT_TERMINATED: HaltType = "terminated";
@@ -16,12 +17,14 @@ const OPCODE_JNZ = 5;
 const OPCODE_JZ = 6;
 const OPCODE_LT = 7;
 const OPCODE_EQ = 8;
+const OPCODE_REL = 9;
 const OPCODE_HLT = 99;
 
 export class IntcodeCPU {
   readonly #program: Memory;
   #memory: Memory;
   #ip = 0;
+  #relativeBase = 0;
   debug = false;
 
   #inputs: Array<number> = [];
@@ -44,6 +47,7 @@ export class IntcodeCPU {
   reset() {
     this.#memory = copyMemory(this.#program);
     this.#ip = 0;
+    this.#relativeBase = 0;
     this.#inputs = [];
     this.#outputs = [];
   }
@@ -116,49 +120,73 @@ export class IntcodeCPU {
     ];
   }
 
-  #read(n: number, mode: Mode): number {
-    return mode === MODE_POSITION ? this.#memory[n] : n;
+  #read(addr: number, mode: Mode): number {
+    const value = this.#memory[addr];
+
+    switch (mode) {
+      case MODE_IMMEDIATE:
+        return value;
+      case MODE_POSITION:
+        return this.#memory[value];
+      case MODE_RELATIVE:
+        return this.#memory[value + this.#relativeBase];
+      default:
+        throw new Error(`Invalid mode: ${mode}`);
+    }
+  }
+
+  #write(addr: number, mode: Mode, value: number) {
+    const target = this.#memory[addr];
+
+    switch (mode) {
+      case MODE_IMMEDIATE:
+      case MODE_POSITION:
+        this.#memory[target] = value;
+        break;
+      case MODE_RELATIVE:
+        this.#memory[this.#relativeBase + target] = value;
+        break;
+    }
   }
 
   //#region OPCODES
 
-  #opAdd(m1: Mode, m2: Mode) {
-    const a = this.#read(this.#memory[this.#ip + 1], m1);
-    const b = this.#read(this.#memory[this.#ip + 2], m2);
+  #opAdd(m1: Mode, m2: Mode, m3: Mode) {
+    const a = this.#read(this.#ip + 1, m1);
+    const b = this.#read(this.#ip + 2, m2);
     const result = a + b;
-    const target = this.#memory[this.#ip + 3];
-    this.#log(`\tADD: ${target} = ${a} + ${b} (${result})`);
+    this.#log(`\tADD: ${a} + ${b} (${result})`);
 
-    this.#memory[target] = result;
+    this.#write(this.#ip + 3, m3, result);
     this.#ip += 4;
   }
 
-  #opMul(m1: Mode, m2: Mode) {
-    const a = this.#read(this.#memory[this.#ip + 1], m1);
-    const b = this.#read(this.#memory[this.#ip + 2], m2);
+  #opMul(m1: Mode, m2: Mode, m3: Mode) {
+    const a = this.#read(this.#ip + 1, m1);
+    const b = this.#read(this.#ip + 2, m2);
     const result = a * b;
-    const target = this.#memory[this.#ip + 3];
-    this.#log(`\tMUL: ${target} = ${a} * ${b} (${result})`);
+    this.#log(`\tMUL: ${a} * ${b} (${result})`);
 
-    this.#memory[target] = result;
+    this.#write(this.#ip + 3, m3, result);
+
     this.#ip += 4;
   }
 
-  #opInp() {
+  #opInp(m1: Mode) {
     if (this.#inputs.length === 0) {
       this.#log(`\tINP: Waiting for input`);
       return HALT_WAITING;
     }
     const input = this.#inputs.shift()!;
-    const target = this.#memory[this.#ip + 1];
-    this.#log(`\tINP: ${target} = ${input}`);
+    this.#log(`\tINP: ${input}`);
 
-    this.#memory[target] = input;
+    this.#write(this.#ip + 1, m1, input);
+
     this.#ip += 2;
   }
 
   #opOut(m1: Mode) {
-    const output = this.#read(this.#memory[this.#ip + 1], m1);
+    const output = this.#read(this.#ip + 1, m1);
     this.#log(`\tOUT: ${output}`);
     this.#outputs.push(output);
 
@@ -166,8 +194,8 @@ export class IntcodeCPU {
   }
 
   #opJnz(m1: Mode, m2: Mode) {
-    const value = this.#read(this.#memory[this.#ip + 1], m1);
-    const addr = this.#read(this.#memory[this.#ip + 2], m2);
+    const value = this.#read(this.#ip + 1, m1);
+    const addr = this.#read(this.#ip + 2, m2);
 
     if (value !== 0) {
       this.#log(`\tJNZ: ${value} != 0  | JUMPING TO ${addr}`);
@@ -179,8 +207,8 @@ export class IntcodeCPU {
   }
 
   #opJz(m1: Mode, m2: Mode) {
-    const value = this.#read(this.#memory[this.#ip + 1], m1);
-    const addr = this.#read(this.#memory[this.#ip + 2], m2);
+    const value = this.#read(this.#ip + 1, m1);
+    const addr = this.#read(this.#ip + 2, m2);
 
     if (value === 0) {
       this.#log(`\tJZ : ${value} == 0  | JUMPING TO ${addr}`);
@@ -191,26 +219,32 @@ export class IntcodeCPU {
     }
   }
 
-  #opLt(m1: Mode, m2: Mode) {
-    const a = this.#read(this.#memory[this.#ip + 1], m1);
-    const b = this.#read(this.#memory[this.#ip + 2], m2);
-    const target = this.#memory[this.#ip + 3];
+  #opLt(m1: Mode, m2: Mode, m3: Mode) {
+    const a = this.#read(this.#ip + 1, m1);
+    const b = this.#read(this.#ip + 2, m2);
     const result = a < b ? 1 : 0;
-    this.#log(`\tLT : ${target} = ${a} < ${b} (${result})`);
+    this.#log(`\tLT : ${a} < ${b} (${result})`);
 
-    this.#memory[target] = result;
+    this.#write(this.#ip + 3, m3, result);
     this.#ip += 4;
   }
 
-  #opEq(m1: Mode, m2: Mode) {
-    const a = this.#read(this.#memory[this.#ip + 1], m1);
-    const b = this.#read(this.#memory[this.#ip + 2], m2);
-    const target = this.#memory[this.#ip + 3];
+  #opEq(m1: Mode, m2: Mode, m3: Mode) {
+    const a = this.#read(this.#ip + 1, m1);
+    const b = this.#read(this.#ip + 2, m2);
     const result = a === b ? 1 : 0;
-    this.#log(`\tEQ : ${target} = ${a} == ${b} (${result})`);
+    this.#log(`\tEQ : ${a} == ${b} (${result})`);
 
-    this.#memory[target] = result;
+    this.#write(this.#ip + 3, m3, result);
     this.#ip += 4;
+  }
+
+  #opRel(m1: Mode) {
+    const offset = this.#read(this.#ip + 1, m1);
+    this.#relativeBase += offset;
+    this.#log(`\tREL: Relative base is now ${this.#relativeBase}`);
+
+    this.#ip += 2;
   }
 
   #opHlt() {
@@ -231,23 +265,23 @@ export class IntcodeCPU {
 
     switch (opcode) {
       case OPCODE_ADD:
-        return this.#opAdd(m1, m2);
+        return this.#opAdd(m1, m2, m3);
       case OPCODE_MUL:
-        return this.#opMul(m1, m2);
+        return this.#opMul(m1, m2, m3);
       case OPCODE_INP:
-        return this.#opInp();
+        return this.#opInp(m1);
       case OPCODE_OUT:
         return this.#opOut(m1);
-
       case OPCODE_JNZ:
         return this.#opJnz(m1, m2);
       case OPCODE_JZ:
         return this.#opJz(m1, m2);
       case OPCODE_LT:
-        return this.#opLt(m1, m2);
-        break;
+        return this.#opLt(m1, m2, m3);
       case OPCODE_EQ:
-        return this.#opEq(m1, m2);
+        return this.#opEq(m1, m2, m3);
+      case OPCODE_REL:
+        return this.#opRel(m1);
       case OPCODE_HLT:
         return this.#opHlt();
       default:
