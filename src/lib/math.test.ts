40710e7b3f88d91a265f6875eac29cbc9fa8d305
Phillip <phillip@praffn.dk>
2025-01-20 14:45:43 +0100

    add gcd function

--- src/lib/math.test.ts
+++ src/lib/math.test.ts
@@ -1,5 +1,5 @@
 import { describe, it } from "node:test";
-import { mod } from "./math";
+import { gcd, mod } from "./math";
 
 describe("lib/math", () => {
   describe("mod", () => {
@@ -17,6 +17,31 @@ describe("lib/math", () => {
           actual,
           expected,
           `expected mod(${a}, ${b}) to be ${expected}, got ${actual}`
+        );
+      }
+    });
+  });
+
+  describe("gcd", () => {
+    it("should return the greatest common divisor", (t) => {
+      const tests = [
+        [48, 18, 6],
+        [18, 48, 6],
+        [48, 0, 48],
+        [0, 48, 48],
+        [0, 0, 0],
+        [1, 1, 1],
+        [1, 0, 1],
+        [7, 3, 1],
+        [13, 7, 1],
+      ] as const;
+
+      for (const [a, b, expected] of tests) {
+        const actual = gcd(a, b);
+        t.assert.equal(
+          actual,
+          expected,
+          `expected gcd(${a}, ${b}) to be ${expected}, got ${actual}`
         );
       }
     });
--- src/lib/math.ts
+++ src/lib/math.ts
@@ -1,3 +1,16 @@
 export function mod(a: number, b: number) {
   return ((a % b) + b) % b;
 }
+
+export function gcd(a: number, b: number) {
+  // iterative was waaaay faster than recursive
+
+  let tmp;
+  while (b !== 0) {
+    tmp = b;
+    b = a % b;
+    a = tmp;
+  }
+
+  return a;
+}
