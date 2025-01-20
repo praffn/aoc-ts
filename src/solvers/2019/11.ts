6fb712da1dfbdd9e8b098df632bf4029d6ee53d7
Phillip <phillip@praffn.dk>
2025-01-20 15:17:43 +0100

    feat: solve 2019 day 11

---
+++ src/solvers/2019/11.ts
@@ -0,0 +1,92 @@
+import { StructuralMap } from "../../lib/collections/structural-map";
+import {
+  add,
+  directions,
+  key,
+  makeVec2,
+  zero,
+  type Vec2,
+} from "../../lib/linalg/vec2";
+import { createSolverWithString } from "../../solution";
+import { HALT_TERMINATED, IntcodeCPU } from "./intcode";
+import { Grid2D } from "../../lib/grid/grid2d";
+
+/**
+ * Turns the direction 90 degrees to the left if turn is 0, otherwise right
+ * (it actually turns right when turning left and vice versa, but that's
+ * because the axes are flipped)
+ */
+function turn90(direction: Vec2, turn: number): Vec2 {
+  return turn === 0
+    ? makeVec2(-direction.y, direction.x)
+    : makeVec2(direction.y, -direction.x);
+}
+
+/**
+ * Given a CPU and a starting color, performs the painting operation
+ * returning a map of positions to colors
+ */
+function performPaint(
+  cpu: IntcodeCPU,
+  startColor: number
+): StructuralMap<Vec2, number> {
+  let currentPosition = zero;
+  let currentDirection = directions.south;
+
+  const paintMap = new StructuralMap(key, [[currentPosition, startColor]]);
+
+  while (true) {
+    cpu.writeInput(paintMap.get(currentPosition) ?? 0);
+    const haltReason = cpu.run();
+
+    const color = cpu.removeFirstOutput();
+    const turn = cpu.removeFirstOutput();
+
+    paintMap.set(currentPosition, color);
+    currentDirection = turn90(currentDirection, turn);
+    currentPosition = add(currentPosition, currentDirection);
+
+    if (haltReason === HALT_TERMINATED) {
+      break;
+    }
+  }
+
+  return paintMap;
+}
+
+/**
+ * Takes a paint map and paints it to a grid, restricting size of only known
+ * painted areas.
+ */
+function paintToGrid(paintMap: StructuralMap<Vec2, number>) {
+  const vecs = Array.from(paintMap.keys());
+  const minX = Math.min(...vecs.map((v) => v.x));
+  const minY = Math.min(...vecs.map((v) => v.y));
+  const maxX = Math.max(...vecs.map((v) => v.x));
+  const maxY = Math.max(...vecs.map((v) => v.y));
+
+  const grid = new Grid2D(maxX - minX + 1, maxY - minY + 1, " ");
+
+  for (const [position, color] of paintMap) {
+    grid.set(position.x - minX, maxY - position.y, color === 1 ? "#" : " ");
+  }
+
+  return grid;
+}
+
+export default createSolverWithString(async (input) => {
+  const cpu = new IntcodeCPU(input);
+
+  const paintMapWithBlackStart = performPaint(cpu, 0);
+
+  // Uncomment below code to print solution to part 2
+  // cpu.reset();
+  // const paintMapWithWhiteStart = performPaint(cpu, 1);
+  // const grid = paintToGrid(paintMapWithWhiteStart);
+  // console.log(grid.toString());
+
+  return {
+    first: paintMapWithBlackStart.size,
+    second: "uncomment and see console",
+  };
+});
