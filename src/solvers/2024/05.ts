0263b0e4c9a852b0ac1a3fffd47804699d05a4c6
Phillip <phillip@praffn.dk>
2025-01-21 15:27:30 +0100

    refactor 2024 day 5 to use directed graph

--- src/solvers/2024/05.ts
+++ src/solvers/2024/05.ts
@@ -1,18 +1,16 @@
+import { DirectedGraph } from "../../lib/graph/directed-graph";
 import { createSolverWithLineArray } from "../../solution";
 
 function isInCorrectOrder(
   production: Array<number>,
-  dependencyGraph: Map<number, Set<number>>
+  dependencyGraph: DirectedGraph<number>
 ): boolean {
   const produced = new Set<number>();
 
   for (const page of production) {
-    const dependencies = dependencyGraph.get(page);
-    if (dependencies) {
-      for (const dependency of dependencies) {
-        if (production.includes(dependency) && !produced.has(dependency)) {
-          return false;
-        }
+    for (const dependency of dependencyGraph.neighbors(page)) {
+      if (production.includes(dependency) && !produced.has(dependency)) {
+        return false;
       }
     }
 
@@ -24,55 +22,21 @@ function isInCorrectOrder(
 
 function order(
   production: Array<number>,
-  dependencyGraph: Map<number, Set<number>>
+  dependencyGraph: DirectedGraph<number>
 ) {
-  const adjacencyList = new Map<number, Set<number>>();
-  const inDegree = new Map<number, number>();
-
-  for (const page of production) {
-    adjacencyList.set(page, new Set());
-    inDegree.set(page, 0);
-  }
-
-  for (const [dependant, dependencies] of dependencyGraph) {
-    adjacencyList.set(dependant, adjacencyList.get(dependant) ?? new Set());
-
-    for (const dependency of dependencies) {
-      if (!production.includes(dependant) || !production.includes(dependency)) {
-        continue;
-      }
-      adjacencyList.set(dependency, adjacencyList.get(dependency) ?? new Set());
-      adjacencyList.get(dependency)!.add(dependant);
-      inDegree.set(dependant, (inDegree.get(dependant) ?? 0) + 1);
+  // create a subgraph of the dependency graph that only includes the
+  // vertices that are in the production
+  const subGraph = new DirectedGraph<number>();
+  for (const [from, to] of dependencyGraph.edges()) {
+    if (!production.includes(from) || !production.includes(to)) {
+      continue;
     }
+
+    subGraph.addEdge(from, to);
   }
 
-  const queue: Array<number> = [];
-  const result: Array<number> = [];
-
-  for (const [dependant, degree] of inDegree) {
-    if (degree === 0) {
-      queue.push(dependant);
-    }
-  }
-
-  while (queue.length > 0) {
-    const current = queue.shift()!;
-    result.push(current);
-
-    const dependants = adjacencyList.get(current)!;
-
-    for (const dependant of dependants) {
-      const degree = inDegree.get(dependant)! - 1;
-      inDegree.set(dependant, degree);
-
-      if (degree === 0) {
-        queue.push(dependant);
-      }
-    }
-  }
-
-  return result;
+  // return the topological sort of the subgraph
+  return Array.from(subGraph.topologicalSort());
 }
 
 export default createSolverWithLineArray(async (input) => {
@@ -85,17 +49,13 @@ export default createSolverWithLineArray(async (input) => {
     .map((line) => line.split(",").map((s) => Number.parseInt(s, 10)));
 
   // parse each ruleset and put into the dependency graph
-  const dependencyGraph = new Map<number, Set<number>>();
+  // const dependencyGraph = new Map<number, Set<number>>();
+  const dependencyGraph = new DirectedGraph<number>();
   for (const rule of rules) {
     const [dependencyStr, dependantStr] = rule.split("|");
     const dependency = Number.parseInt(dependencyStr, 10);
     const dependant = Number.parseInt(dependantStr, 10);
-
-    if (!dependencyGraph.has(dependant)) {
-      dependencyGraph.set(dependant, new Set());
-    }
-
-    dependencyGraph.get(dependant)!.add(dependency);
+    dependencyGraph.addEdge(dependant, dependency);
   }
 
   const validProductions: Array<Array<number>> = [];
@@ -111,7 +71,7 @@ export default createSolverWithLineArray(async (input) => {
 
   let first = 0;
   for (const production of validProductions) {
-    const middleNumber = production[Math.floor(production.length / 2)];
+    const middleNumber = production[production.length >> 1];
     first += middleNumber;
   }
 
