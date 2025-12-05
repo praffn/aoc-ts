c6553c87c2fed1ec2299b1f9573d76ac6b2bea9f
Phillip <phillip@praffn.dk>
2025-12-05 12:48:16 +0100

    feat: add `touches`, `merge`, `overlap` method to `Range`

--- src/lib/range.test.ts
+++ src/lib/range.test.ts
@@ -55,4 +55,46 @@ describe("range", () => {
       t.assert.equal(range.fullyContains(other), false);
     });
   });
+
+  describe("touches", () => {
+    it("should return true if and only if two ranges touch eachother", (t) => {
+      const rangeA = new Range(1, 3);
+      const rangeB = new Range(3, 5);
+
+      t.assert.equal(rangeA.touches(rangeB), true);
+      t.assert.equal(rangeB.touches(rangeA), true);
+    });
+
+    it("should not return true if two ranges overlap", (t) => {
+      const rangeA = new Range(1, 3);
+      const rangeB = new Range(2, 4);
+
+      t.assert.equal(rangeA.touches(rangeB), false);
+      t.assert.equal(rangeB.touches(rangeA), false);
+    });
+
+    it("should not return true if two ranges are not touching", (t) => {
+      const rangeA = new Range(1, 3);
+      const rangeB = new Range(4, 6);
+
+      t.assert.equal(rangeA.touches(rangeB), false);
+      t.assert.equal(rangeB.touches(rangeA), false);
+    });
+  });
+
+  describe("merge", () => {
+    it("should merge two ranges that overlap", (t) => {
+      const rangeA = new Range(1, 3);
+      const rangeB = new Range(2, 4);
+
+      t.assert.equal(rangeA.merge(rangeB).equals(new Range(1, 4)), true);
+    });
+
+    it("should merge two ranges that touch", (t) => {
+      const rangeA = new Range(1, 3);
+      const rangeB = new Range(3, 5);
+
+      t.assert.equal(rangeA.merge(rangeB).equals(new Range(1, 5)), true);
+    });
+  });
 });
--- src/lib/range.ts
+++ src/lib/range.ts
@@ -10,6 +10,14 @@ export class Range {
     this.#end = end;
   }
 
+  get start(): number {
+    return this.#start;
+  }
+
+  get end(): number {
+    return this.#end;
+  }
+
   /**
    * Returns a new Range that is inclusive of the end value, i.e. [start, end]
    */
@@ -21,7 +29,7 @@ export class Range {
    * Returns the total number of values in the range
    */
   length(): number {
-    return this.#end - this.#start + 1;
+    return this.#end - this.#start;
   }
 
   /**
@@ -35,7 +43,26 @@ export class Range {
    * Returns true if the given range overlaps with this range
    */
   overlaps(other: Range): boolean {
-    return this.contains(other.#start) || other.contains(this.#start);
+    return this.#start < other.#end && this.#end > other.#start;
+    // return this.contains(other.#start) || other.contains(this.#start);
+  }
+
+  /**
+   * Returns true if the given range touches but does not overlap with this range
+   */
+  touches(other: Range): boolean {
+    return this.#end === other.#start || this.#start === other.#end;
+  }
+
+  /**
+   * Merges two ranges. It is the caller's responsibility to ensure that ranges
+   * either overlap or touch.
+   */
+  merge(other: Range): Range {
+    return new Range(
+      Math.min(this.#start, other.#start),
+      Math.max(this.#end, other.#end)
+    );
   }
 
   /**
@@ -43,6 +70,14 @@ export class Range {
    */
   fullyContains(other: Range): boolean {
     return this.contains(other.#start) && this.contains(other.#end - 1);
+  }
+
+  equals(other: unknown): boolean {
+    return (
+      other instanceof Range &&
+      this.#start === other.#start &&
+      this.#end === other.#end
+    );
   }
 
   *[Symbol.iterator]() {
